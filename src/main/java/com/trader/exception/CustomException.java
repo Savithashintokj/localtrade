0a9ede9d0771e330e013bf3e817666205ddadeb2
Savitha Shinto <savitha.mylswami@gmail.com>
2025-08-13 17:03:22 +0100

    Added trader, user and otp

--- src/main/java/com/trader/exception/CustomException.java
+++ src/main/java/com/trader/exception/CustomException.java
@@ -1,2 +1,6 @@
-package com.trader.exception;public class CustomException {
+package com.trader.exception;
+
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) { super(message); }
 }
